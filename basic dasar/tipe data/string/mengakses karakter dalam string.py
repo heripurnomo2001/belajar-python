8a226e2fe242f71de3b5603d7e897e0d828db8d0
Heri Purnomo <P0159@ERL.DC.NET>
2024-04-18 10:07:28 +0700

    memperbaiki code Mengakses karakter dalam string

--- basic dasar/tipe data/string/mengakses karakter dalam string.py
+++ basic dasar/tipe data/string/mengakses karakter dalam string.py
@@ -1,36 +1,51 @@
 # MENGAKSES KARAKTER DALAM STRING
 
-# entry form user
-while True:
-    try:
+
+# function
+def input_kata():
+    while True:
         kata = input("Masukkan sebuah kata (ketik 'quit' untuk keluar): ")
-        if kata.isspace():  # bisa juga dengan : if kata.strip() == ""
+        if kata.strip() == "":
             print("kata tidak boleh kosong.")
         elif kata.lower() == 'quit':
-            break
+            return None
         else:
-            panjang_kata = len(kata.strip())
-            print("Panjang kata adalah ", panjang_kata, "karakter")
-            print("Catatan, spasi kanan dan kiri kata diabaikan.\n")
-            n = input(f"Masukkan angka ke n untuk menampilkan karakter ke n. Angka >=0 dan <= {panjang_kata-1}: ")
-            try:
-                n = int(n)
-            except ValueError as e:
-                print("Data yang anda masukkan tidak valid: ", e)
-                
-            karakter_ke_n = kata[n]
+            return kata
 
-            # cetak karakter ke n
-            print("Karakter ke n adalah : ", karakter_ke_n)
-
-            confirm_lanjut = input("Apakah anda ingin melanjutkan kata berikutnya? (y/n): ").lower()
-            if confirm_lanjut == 'y':
-                pass
-            elif confirm_lanjut == 'n':
-                print("\nTerima kasih sudah berlatih pemrograman python hari ini!")
-                break
+def input_indeks(panjang_kata):
+    while True:
+        try:
+            n = int(input(f"Masukkan angka ke n untuk menampilkan karakter ke n. Angka >=0 dan <= {panjang_kata-1}: "))            
+            if 0 <= n <= panjang_kata-1:
+                return n # mengembalikan indeks yang valid
             else:
-                print("input yang anda masukkan tidak valid!")
+                print(f"Angka yang dimasukkan harus di antara 0 dan {panjang_kata-1}.")
+
+        except ValueError:
+            print("Masukkan angka yang valid!")
+
+# entry form user
+while True:
+    kata = input_kata()
+    if kata == None:
+        break
+    
+    panjang_kata = len(kata)        
+    print("Panjang kata adalah ", panjang_kata, "karakter")
+
+
+    n = input_indeks(panjang_kata)                
+    karakter_ke_n = kata[n]
+
+    # cetak karakter ke n
+    print(f"Karakter ke {n} adalah : {karakter_ke_n}")
+
+    confirm_lanjut = input("Apakah anda ingin melanjutkan kata berikutnya? (y/n): ").lower()
+    if confirm_lanjut == 'y':  
+        pass
+    elif confirm_lanjut == 'n':
+        print("\nTerima kasih sudah berlatih pemrograman python hari ini!")
+        break
+    else:
+        print("Pilihan hanya 'y' atau 'n'!")
             
-    except ValueError as e:
-        print("Data yang anda masukkan tidak valid! Kesalahan : ", e)
