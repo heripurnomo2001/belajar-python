7476bdd629d44ccab040c76a40f9aa2da7eaa6ac
Heri Purnomo <P0159@ERL.DC.NET>
2024-04-19 08:57:37 +0700

    menambah 2 program : akses karakter ...

---
+++ basic dasar/tipe data/string/mengecek kehadiran karakter dalam string.py
@@ -0,0 +1,8 @@
+'''
+Soal 5: Mengecek Kehadiran Karakter dalam String
+Buatlah sebuah program Python yang meminta pengguna untuk memasukkan sebuah string dan sebuah karakter.
+Periksa apakah karakter tersebut ada dalam string.
+Jika ada, cetak "Karakter ditemukan dalam string."
+Jika tidak, cetak "Karakter tidak ditemukan dalam string."
+'''
+
